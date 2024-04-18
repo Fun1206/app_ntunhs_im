98be12de79cf04422a7357c1346a5ec4d11b5e75
Fun1206 <imfiona719@gmail.com>
2024-04-18 16:37:47 +0800

    20240418

--- Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
+++ Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
@@ -4,84 +4,55 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
-import java.util.*
-
+import com.example.guess_number.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     val TAG:String = MainActivity::class.java.simpleName
     private lateinit var handler: Handler
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var model: GameModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         handler = Handler(Looper.getMainLooper())
-        val textView1 = findViewById<TextView>(R.id.textView1)//
-        val edit_num = findViewById<EditText>(R.id.edit_num)
-        val button1 = findViewById<Button>(R.id.button1)//guess
-        val button2 = findViewById<Button>(R.id.button2)//reset
-        val textView2 = findViewById<TextView>(R.id.textView2)//區間
-
-        var validate_num:Int
-        var min:Int=1
-        var max:Int=100
-
-        var secert: Int = Random().nextInt(100)+1//隨機1-100取一數
-
-        /*handler.postDelayed({
-            Toast.makeText(this,"5秒後的操作執行了",Toast.LENGTH_SHORT).show()
-        },5000)
-        */
-
-        /*button1.setOnClickListener{
-            textView1.text=name.text
-        }*/
-        edit_num.text.clear()
-        //textView1.text=secert.toString()
-        textView2.text=min.toString()+"-"+max.toString()
-        button1.setOnClickListener{
-            validate_num=edit_num.text.toString().toInt()-secert
-
-            if(validate_num>0)
-            {
-                textView1.text="請猜小一點 !!!"
-                max=edit_num.text.toString().toInt()
-                textView2.text=min.toString()+"-"+max.toString()
+        model = GameModel()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.editnum.text.clear()
+        binding.button1.setOnClickListener{
+            val guess = binding.editnum.text.toString().toInt()
+            when {
+                model.isGuessTooLow(guess) -> {
+                    model.min = guess
+                    binding.textView1.text = "你猜得太小囉!"
+                    binding.textView2.text = "${model.min}-${model.max}"
+                }
+                model.isGuessTooHigh(guess) -> {
+                    model.max = guess
+                    binding.textView1.text = "你猜得太大囉!"
+                    binding.textView2.text = "${model.min}-${model.max}"
+                }
+                else -> {
+                    binding.textView1.text = "你猜對了!!"
+                    binding.textView2.text = "正確數字是${model.secret}"
+                    Toast.makeText(this, "6秒後重置遊戲", Toast.LENGTH_SHORT).show()
+                    handler.postDelayed({
+                        model.resetGame()
+                        binding.textView1.text = "重新開始猜數字"
+                        binding.textView2.text = "${model.min}-${model.max}"
+                        binding.editnum.text.clear()
+                    }, 6000)
+                }
             }
-            else if(validate_num<0)
-            {
-                textView1.text="請猜大一點 !!!"
-                min=edit_num.text.toString().toInt()
-                textView2.text=min.toString()+"-"+max.toString()
-            }
-            else
-            {
-                textView1.text="恭喜猜對了 !!!"
-                Toast.makeText(this,"~ 6秒後重置遊戲 ~",Toast.LENGTH_SHORT).show()
-                handler.postDelayed({
-                    edit_num.text.clear()
-                    min=1
-                    max=100
-                    secert = Random().nextInt(100)+1//隨機1-100取一數
-                    //textView1.text=secert.toString()
-                    textView2.text=min.toString()+"-"+max.toString()
-                },6000)
-            }
-
         }
-        button2.setOnClickListener{
-            edit_num.text.clear()
-            min=1
-            max=100
-            secert = Random().nextInt(100)+1//隨機1-100取一數
-            //textView1.text=secert.toString()
-            textView2.text=min.toString()+"-"+max.toString()
+        binding.button2.setOnClickListener{
+            model.resetGame()
+            binding.textView1.text = "重新開始猜數字"
+            binding.textView2.text = "${model.min}-${model.max}"
+            binding.editnum.text.clear()
         }
-
     }
-
     override fun onDestroy() {
         super.onDestroy()
         handler.removeCallbacksAndMessages(null)
