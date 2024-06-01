35751d4922059ef34785f93111b24a6d6b12d6f8
toruuuu <ghost.toru@gmail.com>
2024-06-01 18:30:02 +0800

    06011830

---
+++ team8/app/src/main/java/com/example/consultationclinicapp/search_drugs.kt
@@ -0,0 +1,11 @@
+package com.example.consultationclinicapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class search_drugs : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_drugs)
+    }
+}
